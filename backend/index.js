8f7486ac54383a4048a9b4ffddf02c79080f73de
viswanathr73 <viswanathr73@gmail.com>
2025-12-01 12:36:41 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import mongoose from "mongoose";
+import cors from "cors";
+import { serve } from "inngest/express";
+import userRoutes from "./routes/user.js";
+import ticketRoutes from "./routes/ticket.js";
+import { inngest } from "./inngest/client.js";
+import { onUserSignup } from "./inngest/functions/on-signup.js";
+import { onTicketCreated } from "./inngest/functions/on-ticket-create.js";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+
+
+const PORT = process.env.PORT || 3000
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+app.use("/api/auth", userRoutes);
+app.use("/api/tickets", ticketRoutes);
+
+app.use(
+    "/api/inngest",
+    serve({
+        client: inngest,
+        functions: [onUserSignup, onTicketCreated],
+    })
+);
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("MongoDB Connected");
+        app.listen(PORT, () => console.log("Server at http://localhost:3000"));
+
+    })
+    .catch((err) => console.error("MongoDB error:", err));
---
+++ backend/models/user.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, default: "User", enum: ["user", "moderator", "admin"] },
+    skills: [String],
+    createdAt: { type: Date, default: Date.now }
+
+})
+
+export default mongoose.model("User", userSchema)
